2a4091858780f10abdb26a3d0634d1f4651e51eb
Mungui <aliemre.sirius@gmail.com>
2018-12-24 06:52:08 +0300

    Initial commit

---
+++ Engine.cpp
@@ -0,0 +1,14 @@
+﻿// Engine.cpp : Defines the entry point for the application.
+//
+
+#include "Engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	int a;
+	scanf("%d", &a);
+	return 0;
+}
